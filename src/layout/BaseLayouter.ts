886e464af83c7c5b9b636be6fe8dda4f735b1732
joern.otte@gmx.net <joern.otte@gmx.net>
2018-04-21 21:05:53 +0200

    BaseLayouter.ts

--- src/layout/BaseLayouter.ts
+++ src/layout/BaseLayouter.ts
@@ -1,5 +1,5 @@
 import {
-  getMid
+	getMid
 } from './LayoutUtil';
 
 
@@ -8,28 +8,30 @@ import {
  * that layouts the connection by directly connecting
  * mid(source) + mid(target).
  */
-export default function BaseLayouter() {}
+export default class BaseLayouter {
+	/**
+	 * Return the new layouted waypoints for the given connection.
+	 *
+	 * The connection passed is still unchanged; you may figure out about
+	 * the new connection start / end via the layout hints provided.
+	 *
+	 * @param {djs.model.Connection} connection
+	 * @param {Object} [hints]
+	 * @param {Point} [hints.connectionStart]
+	 * @param {Point} [hints.connectionEnd]
+	 *
+	 * @return {Array<Point>} the layouted connection waypoints
+	 */
+	public layoutConnection(connection : any, hints : any) {
+
+		hints = hints || {};
+
+		return [
+			hints.connectionStart || getMid(connection.source),
+			hints.connectionEnd || getMid(connection.target)
+		];
+	};
+}
 
 
-/**
- * Return the new layouted waypoints for the given connection.
- *
- * The connection passed is still unchanged; you may figure out about
- * the new connection start / end via the layout hints provided.
- *
- * @param {djs.model.Connection} connection
- * @param {Object} [hints]
- * @param {Point} [hints.connectionStart]
- * @param {Point} [hints.connectionEnd]
- *
- * @return {Array<Point>} the layouted connection waypoints
- */
-BaseLayouter.prototype.layoutConnection = function(connection, hints) {
 
-  hints = hints || {};
-
-  return [
-    hints.connectionStart || getMid(connection.source),
-    hints.connectionEnd || getMid(connection.target)
-  ];
-};
