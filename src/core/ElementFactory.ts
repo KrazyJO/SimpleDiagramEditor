8288d0959127e888baf711a74aded4510002d0fb
joern.otte@gmx.net <joern.otte@gmx.net>
2018-04-21 14:17:54 +0200

    ElementFactory.ts

--- src/core/ElementFactory.ts
+++ src/core/ElementFactory.ts
@@ -1,5 +1,5 @@
 import {
-  create
+	create
 } from '../model';
 
 import { assign } from 'min-dash';
@@ -7,42 +7,48 @@ import { assign } from 'min-dash';
 /**
  * A factory for diagram-js shapes
  */
-export default function ElementFactory() {
-  this._uid = 12;
+export default class ElementFactory {
+
+	_uid: number;
+
+	constructor() {
+		this._uid = 12;
+	}
+
+	public createRoot(attrs: any) {
+		return this.create('root', attrs);
+	};
+
+	public createLabel(attrs: any) {
+		return this.create('label', attrs);
+	};
+
+	public createShape(attrs: any) {
+		return this.create('shape', attrs);
+	};
+
+	public createConnection(attrs: any) {
+		return this.create('connection', attrs);
+	};
+
+	/**
+	 * Create a model element with the given type and
+	 * a number of pre-set attributes.
+	 *
+	 * @param  {String} type
+	 * @param  {Object} attrs
+	 * @return {djs.model.Base} the newly created model instance
+	 */
+	public create(type: string, attrs: any) {
+
+		attrs = assign({}, attrs || {});
+
+		if (!attrs.id) {
+			attrs.id = type + '_' + (this._uid++);
+		}
+
+		return create(type, attrs);
+	};
 }
 
 
-ElementFactory.prototype.createRoot = function(attrs) {
-  return this.create('root', attrs);
-};
-
-ElementFactory.prototype.createLabel = function(attrs) {
-  return this.create('label', attrs);
-};
-
-ElementFactory.prototype.createShape = function(attrs) {
-  return this.create('shape', attrs);
-};
-
-ElementFactory.prototype.createConnection = function(attrs) {
-  return this.create('connection', attrs);
-};
-
-/**
- * Create a model element with the given type and
- * a number of pre-set attributes.
- *
- * @param  {String} type
- * @param  {Object} attrs
- * @return {djs.model.Base} the newly created model instance
- */
-ElementFactory.prototype.create = function(type, attrs) {
-
-  attrs = assign({}, attrs || {});
-
-  if (!attrs.id) {
-    attrs.id = type + '_' + (this._uid++);
-  }
-
-  return create(type, attrs);
-};
