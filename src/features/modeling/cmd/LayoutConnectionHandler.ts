3601f69baeb6acf5c4007268ec3b911b328f03ac
joern.otte@gmx.net <joern.otte@gmx.net>
2018-04-28 12:25:46 +0200

    t´scripted LayoutConnectionHandler

--- src/features/modeling/cmd/LayoutConnectionHandler.ts
+++ src/features/modeling/cmd/LayoutConnectionHandler.ts
@@ -1,36 +1,49 @@
 import { assign } from 'min-dash';
+import BaseLayouter from '../../../layout/BaseLayouter';
+import Canvas from '../../../core/Canvas';
 
 
 /**
  * A handler that implements reversible moving of shapes.
  */
-export default function LayoutConnectionHandler(layouter, canvas) {
-  this._layouter = layouter;
-  this._canvas = canvas;
+export default class LayoutConnectionHandler {
+
+	public static $inject = ['layouter', 'canvas'];
+
+	private _layouter: BaseLayouter;
+	private _canvas: Canvas;
+
+	constructor(layouter: BaseLayouter, canvas: Canvas) {
+		this._layouter = layouter;
+		this._canvas = canvas;
+
+	}
+
+	public execute(context: any): any {
+
+		var connection = context.connection;
+
+		var oldWaypoints = connection.waypoints;
+
+		assign(context, {
+			oldWaypoints: oldWaypoints
+		});
+
+		connection.waypoints = this._layouter.layoutConnection(connection, context.hints);
+
+		return connection;
+	};
+
+	public revert(context: any): any {
+
+		var connection = context.connection;
+
+		connection.waypoints = context.oldWaypoints;
+
+		return connection;
+	};
+
+
 }
 
-LayoutConnectionHandler.$inject = [ 'layouter', 'canvas' ];
 
-LayoutConnectionHandler.prototype.execute = function(context) {
-
-  var connection = context.connection;
-
-  var oldWaypoints = connection.waypoints;
-
-  assign(context, {
-    oldWaypoints: oldWaypoints
-  });
-
-  connection.waypoints = this._layouter.layoutConnection(connection, context.hints);
-
-  return connection;
-};
-
-LayoutConnectionHandler.prototype.revert = function(context) {
-
-  var connection = context.connection;
-
-  connection.waypoints = context.oldWaypoints;
-
-  return connection;
-};
