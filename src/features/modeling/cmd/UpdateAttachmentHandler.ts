ec936ddfaaceb6c5feea566b82304655ee96df4e
joern.otte@gmx.net <joern.otte@gmx.net>
2018-04-28 12:58:30 +0200

    t´scripted UpdateAttachmentHandler

--- src/features/modeling/cmd/UpdateAttachmentHandler.ts
+++ src/features/modeling/cmd/UpdateAttachmentHandler.ts
@@ -2,60 +2,64 @@ import {
   add as collectionAdd,
   remove as collectionRemove
 } from '../../../util/Collections';
+import Modeling from '../Modeling';
 
 /**
  * A handler that implements reversible attaching/detaching of shapes.
  */
-export default function UpdateAttachmentHandler(modeling) {
-  this._modeling = modeling;
+export default class UpdateAttachmentHandler {
+  
+	public _modeling : Modeling;
+	public static $inject = [ 'modeling' ];
+
+	constructor(modeling : Modeling) {
+	  this._modeling = modeling;
+  }
+
+  public execute(context : any) {
+	var shape = context.shape,
+		newHost = context.newHost,
+		oldHost = shape.host;
+  
+	// (0) detach from old host
+	context.oldHost = oldHost;
+	context.attacherIdx = removeAttacher(oldHost, shape);
+  
+	// (1) attach to new host
+	addAttacher(newHost, shape);
+  
+	// (2) update host
+	shape.host = newHost;
+  
+	return shape;
+  };
+  
+  public revert(context : any) {
+	var shape = context.shape,
+		newHost = context.newHost,
+		oldHost = context.oldHost,
+		attacherIdx = context.attacherIdx;
+  
+	// (2) update host
+	shape.host = oldHost;
+  
+	// (1) attach to new host
+	removeAttacher(newHost, shape);
+  
+	// (0) detach from old host
+	addAttacher(oldHost, shape, attacherIdx);
+  
+	return shape;
+  };
 }
 
-UpdateAttachmentHandler.$inject = [ 'modeling' ];
 
-
-UpdateAttachmentHandler.prototype.execute = function(context) {
-  var shape = context.shape,
-      newHost = context.newHost,
-      oldHost = shape.host;
-
-  // (0) detach from old host
-  context.oldHost = oldHost;
-  context.attacherIdx = removeAttacher(oldHost, shape);
-
-  // (1) attach to new host
-  addAttacher(newHost, shape);
-
-  // (2) update host
-  shape.host = newHost;
-
-  return shape;
-};
-
-UpdateAttachmentHandler.prototype.revert = function(context) {
-  var shape = context.shape,
-      newHost = context.newHost,
-      oldHost = context.oldHost,
-      attacherIdx = context.attacherIdx;
-
-  // (2) update host
-  shape.host = oldHost;
-
-  // (1) attach to new host
-  removeAttacher(newHost, shape);
-
-  // (0) detach from old host
-  addAttacher(oldHost, shape, attacherIdx);
-
-  return shape;
-};
-
-
-function removeAttacher(host, attacher) {
+function removeAttacher(host : any, attacher : any) {
   // remove attacher from host
   return collectionRemove(host && host.attachers, attacher);
 }
 
-function addAttacher(host, attacher, idx) {
+function addAttacher(host : any, attacher : any, idx? : number) {
 
   if (!host) {
     return;
