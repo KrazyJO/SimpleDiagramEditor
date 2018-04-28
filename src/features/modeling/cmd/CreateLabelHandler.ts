019d19d2499417dabd1ec43108760f52f3b87bdb
joern.otte@gmx.net <joern.otte@gmx.net>
2018-04-28 12:05:10 +0200

    t´scripted CreateLabelHander & CreateShapeHandler

--- src/features/modeling/cmd/CreateLabelHandler.ts
+++ src/features/modeling/cmd/CreateLabelHandler.ts
@@ -1,28 +1,24 @@
-import inherits from 'inherits';
-
 import CreateShapeHandler from './CreateShapeHandler';
+import Canvas from '../../../core/Canvas';
 
+
+let originalExecute = CreateShapeHandler.prototype.execute;
+let originalRevert = CreateShapeHandler.prototype.revert;
 
 /**
  * A handler that attaches a label to a given target shape.
  *
  * @param {Canvas} canvas
  */
-export default function CreateLabelHandler(canvas) {
-  CreateShapeHandler.call(this, canvas);
-}
+export default class CreateLabelHandler extends CreateShapeHandler {
 
-inherits(CreateLabelHandler, CreateShapeHandler);
+	public static $inject = ['canvas'];
 
-CreateLabelHandler.$inject = [ 'canvas' ];
-
-
-// api //////////////////////
-
-
-var originalExecute = CreateShapeHandler.prototype.execute;
-
-/**
+	constructor(canvas: Canvas) {
+		super(canvas)
+		// CreateShapeHandler.call(this, canvas);
+	}
+	/**
  * Appends a label to a target shape.
  *
  * @method CreateLabelHandler#execute
@@ -32,36 +28,34 @@ var originalExecute = CreateShapeHandler.prototype.execute;
  * @param {ElementDescriptor} context.parent the parent object
  * @param {Point} context.position position of the new element
  */
-CreateLabelHandler.prototype.execute = function(context) {
+	public execute(context: any) {
 
-  var label = context.shape;
+		var label = context.shape;
 
-  ensureValidDimensions(label);
+		ensureValidDimensions(label);
 
-  label.labelTarget = context.labelTarget;
+		label.labelTarget = context.labelTarget;
 
-  return originalExecute.call(this, context);
-};
+		return originalExecute.call(this, context);
+	};
 
-var originalRevert = CreateShapeHandler.prototype.revert;
+	/**
+	 * Undo append by removing the shape
+	 */
+	public revert(context: any) {
+		context.shape.labelTarget = null;
 
-/**
- * Undo append by removing the shape
- */
-CreateLabelHandler.prototype.revert = function(context) {
-  context.shape.labelTarget = null;
-
-  return originalRevert.call(this, context);
-};
+		return originalRevert.call(this, context);
+	};
 
 
-// helpers //////////////////////
+}
 
-function ensureValidDimensions(label) {
-  // make sure a label has valid { width, height } dimensions
-  [ 'width', 'height' ].forEach(function(prop) {
-    if (typeof label[prop] === 'undefined') {
-      label[prop] = 0;
-    }
-  });
+function ensureValidDimensions(label: any) {
+	// make sure a label has valid { width, height } dimensions
+	['width', 'height'].forEach(function (prop) {
+		if (typeof label[prop] === 'undefined') {
+			label[prop] = 0;
+		}
+	});
 }
--- src/features/modeling/cmd/CreateShapeHandler.ts
+++ src/features/modeling/cmd/CreateShapeHandler.ts
@@ -1,4 +1,5 @@
 import { assign } from 'min-dash';
+import Canvas from '../../../core/Canvas';
 
 var round = Math.round;
 
@@ -8,12 +9,15 @@ var round = Math.round;
  *
  * @param {canvas} Canvas
  */
-export default function CreateShapeHandler(canvas) {
-  this._canvas = canvas;
-}
+export default class CreateShapeHandler {
+	
+	public static $inject = ['canvas'];
+	private _canvas : Canvas;
 
-CreateShapeHandler.$inject = [ 'canvas' ];
-
+	constructor(canvas : Canvas) {
+		this._canvas = canvas;
+	}
+	
 
 // api //////////////////////
 
@@ -25,43 +29,44 @@ CreateShapeHandler.$inject = [ 'canvas' ];
  * @param {djs.model.Base} context.parent the parent object
  * @param {Point} context.position position of the new element
  */
-CreateShapeHandler.prototype.execute = function(context) {
+public execute(context : any ) : void {
 
-  var shape = context.shape,
-      positionOrBounds = context.position,
-      parent = context.parent,
-      parentIndex = context.parentIndex;
+	var shape = context.shape,
+		positionOrBounds = context.position,
+		parent = context.parent,
+		parentIndex = context.parentIndex;
 
-  if (!parent) {
-    throw new Error('parent required');
-  }
+	if (!parent) {
+		throw new Error('parent required');
+	}
 
-  if (!positionOrBounds) {
-    throw new Error('position required');
-  }
+	if (!positionOrBounds) {
+		throw new Error('position required');
+	}
 
-  // (1) add at event center position _or_ at given bounds
-  if (positionOrBounds.width !== undefined) {
-    assign(shape, positionOrBounds);
-  } else {
-    assign(shape, {
-      x: positionOrBounds.x - round(shape.width / 2),
-      y: positionOrBounds.y - round(shape.height / 2)
-    });
-  }
+	// (1) add at event center position _or_ at given bounds
+	if (positionOrBounds.width !== undefined) {
+		assign(shape, positionOrBounds);
+	} else {
+		assign(shape, {
+			x: positionOrBounds.x - round(shape.width / 2),
+			y: positionOrBounds.y - round(shape.height / 2)
+		});
+	}
 
-  // (2) add to canvas
-  this._canvas.addShape(shape, parent, parentIndex);
+	// (2) add to canvas
+	this._canvas.addShape(shape, parent, parentIndex);
 
-  return shape;
+	return shape;
 };
 
 
 /**
  * Undo append by removing the shape
  */
-CreateShapeHandler.prototype.revert = function(context) {
+public revert(context : any) : void {
 
-  // (3) remove form canvas
-  this._canvas.removeShape(context.shape);
+	// (3) remove form canvas
+	this._canvas.removeShape(context.shape);
 };
+}
