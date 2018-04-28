373f978f9be4fe1dd59dfd98b6a102fdd4d39df6
joern.otte@gmx.net <joern.otte@gmx.net>
2018-04-28 12:29:44 +0200

    t´scripted MoveElementsHandler

--- src/features/modeling/cmd/MoveElementsHandler.ts
+++ src/features/modeling/cmd/MoveElementsHandler.ts
@@ -1,34 +1,42 @@
 import MoveHelper from './helper/MoveHelper';
+import Modeling from '../Modeling';
 
 
 /**
  * A handler that implements reversible moving of shapes.
  */
-export default function MoveElementsHandler(modeling) {
-  this._helper = new MoveHelper(modeling);
+export default class MoveElementsHandler {
+
+	private _helper: MoveHelper;
+
+	public static $inject = ['modeling'];
+
+	constructor(modeling: Modeling) {
+		this._helper = new MoveHelper(modeling);
+	}
+
+	public preExecute(context: any): void {
+		context.closure = this._helper.getClosure(context.shapes);
+	};
+
+	public postExecute(context: any): void {
+
+		var hints = context.hints,
+			primaryShape;
+
+		if (hints && hints.primaryShape) {
+			primaryShape = hints.primaryShape;
+			hints.oldParent = primaryShape.parent;
+		}
+
+		this._helper.moveClosure(
+			context.closure,
+			context.delta,
+			context.newParent,
+			context.newHost,
+			primaryShape
+		);
+	};
 }
 
-MoveElementsHandler.$inject = [ 'modeling' ];
 
-MoveElementsHandler.prototype.preExecute = function(context) {
-  context.closure = this._helper.getClosure(context.shapes);
-};
-
-MoveElementsHandler.prototype.postExecute = function(context) {
-
-  var hints = context.hints,
-      primaryShape;
-
-  if (hints && hints.primaryShape) {
-    primaryShape = hints.primaryShape;
-    hints.oldParent = primaryShape.parent;
-  }
-
-  this._helper.moveClosure(
-    context.closure,
-    context.delta,
-    context.newParent,
-    context.newHost,
-    primaryShape
-  );
-};
