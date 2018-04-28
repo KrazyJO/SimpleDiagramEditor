2d3ec64659f46f5b9c7cebf77a03d8a4d94066cc
joern.otte@gmx.net <joern.otte@gmx.net>
2018-04-28 21:33:47 +0200

    t´scripted Styles

--- src/draw/Styles.ts
+++ src/draw/Styles.ts
@@ -8,66 +8,75 @@ import {
 /**
  * A component that manages shape styles
  */
-export default function Styles() {
+export default class Styles {
 
-	var defaultTraits = {
 
-		'no-fill': {
-			fill: 'none'
-		},
-		'no-border': {
-			strokeOpacity: 0.0
-		},
-		'no-events': {
-			pointerEvents: 'none'
-		}
-	};
+	public cls : Function;
+	public style : Function;
+	public computeStyle : Function;
 
-	var self = this;
+	constructor() {
+		var defaultTraits = {
 
-	/**
-	 * Builds a style definition from a className, a list of traits and an object of additional attributes.
-	 *
-	 * @param  {String} className
-	 * @param  {Array<String>} traits
-	 * @param  {Object} additionalAttrs
-	 *
-	 * @return {Object} the style defintion
-	 */
-	this.cls = function (className: string, traits: string[], additionalAttrs: any): any {
-		var attrs = this.style(traits, additionalAttrs);
+			'no-fill': {
+				fill: 'none'
+			},
+			'no-border': {
+				strokeOpacity: 0.0
+			},
+			'no-events': {
+				pointerEvents: 'none'
+			}
+		};
+	
+		var self = this;
+	
+		/**
+		 * Builds a style definition from a className, a list of traits and an object of additional attributes.
+		 *
+		 * @param  {String} className
+		 * @param  {Array<String>} traits
+		 * @param  {Object} additionalAttrs
+		 *
+		 * @return {Object} the style defintion
+		 */
+		this.cls = function(className: string, traits: string[], additionalAttrs: any): any {
+			var attrs = this.style(traits, additionalAttrs);
+	
+			return assign(attrs, { 'class': className });
+		};
+	
+		/**
+		 * Builds a style definition from a list of traits and an object of additional attributes.
+		 *
+		 * @param  {Array<String>} traits
+		 * @param  {Object} additionalAttrs
+		 *
+		 * @return {Object} the style defintion
+		 */
+		this.style = function (traits: string[], additionalAttrs: any): any {
+	
+			if (!isArray(traits) && !additionalAttrs) {
+				additionalAttrs = traits;
+				traits = [];
+			}
+	
+			var attrs = reduce(traits, function (attrs: any, t: any) {
+				return assign(attrs, defaultTraits[t] || {});
+			}, {});
+	
+			return additionalAttrs ? assign(attrs, additionalAttrs) : attrs;
+		};
+	
+		this.computeStyle = function (custom: any, traits: string[], defaultStyles: any) {
+			if (!isArray(traits)) {
+				defaultStyles = traits;
+				traits = [];
+			}
+	
+			return self.style(traits || [], assign({}, defaultStyles, custom || {}));
+		};
+	}
 
-		return assign(attrs, { 'class': className });
-	};
-
-	/**
-	 * Builds a style definition from a list of traits and an object of additional attributes.
-	 *
-	 * @param  {Array<String>} traits
-	 * @param  {Object} additionalAttrs
-	 *
-	 * @return {Object} the style defintion
-	 */
-	this.style = function (traits: string[], additionalAttrs: any): any {
-
-		if (!isArray(traits) && !additionalAttrs) {
-			additionalAttrs = traits;
-			traits = [];
-		}
-
-		var attrs = reduce(traits, function (attrs: any, t: any) {
-			return assign(attrs, defaultTraits[t] || {});
-		}, {});
-
-		return additionalAttrs ? assign(attrs, additionalAttrs) : attrs;
-	};
-
-	this.computeStyle = function (custom: any, traits: string[], defaultStyles: any) {
-		if (!isArray(traits)) {
-			defaultStyles = traits;
-			traits = [];
-		}
-
-		return self.style(traits || [], assign({}, defaultStyles, custom || {}));
-	};
+	
 }
