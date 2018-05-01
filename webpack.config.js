bc0ea1e0d2c2d15abeae4d1359a014e4f2358776
joern.otte@gmx.net <joern.otte@gmx.net>
2018-05-01 07:04:06 +0200

    added webpack image-loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,18 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
-       }
+       },
+       {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }, 'image-webpack-loader'
+        ]
+      }
     ]
   },
   resolve: {
