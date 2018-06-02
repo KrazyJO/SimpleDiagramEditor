5385562a0d7c4c422c092c235e8aa76c2afea15a
joern.otte@gmx.net <joern.otte@gmx.net>
2018-06-02 13:33:41 +0200

    added path @diagram-ts for easier imports

--- src/features/context-pad/index.ts
+++ src/features/context-pad/index.ts
@@ -6,13 +6,13 @@ export default {
   __depends__: [
     // require('diagram-js-direct-editing'),
     // require('diagram-js/lib/features/context-pad'),
-    require('../../diagram-ts/features/context-pad'),
+    require('@diagram-ts/features/context-pad'),
     // require('diagram-js/lib/features/selection'),
-    require('../../diagram-ts/features/selection').default,
+    require('@diagram-ts/features/selection').default,
     // require('diagram-js/lib/features/connect'),
-    require('../../diagram-ts/features/connect'),
+    require('@diagram-ts/features/connect'),
     // require('diagram-js/lib/features/create'),
-    require('../../diagram-ts/features/create')
+    require('@diagram-ts/features/create')
   ],
   __init__: ['contextPadProvider'],
   contextPadProvider: ['type', ContextPadProvider]
