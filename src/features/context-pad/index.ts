e4dfa37e7a9dc97b789001c55302a94a5dac9eaf
joern.otte@gmx.net <joern.otte@gmx.net>
2018-06-02 15:01:23 +0200

    added and modified diagram-js-direct-editing

--- src/features/context-pad/index.ts
+++ src/features/context-pad/index.ts
@@ -4,7 +4,7 @@ import ContextPadProvider from './ContextPadProvider';
 //---------------------EXPORTS---------------------
 export default {
   __depends__: [
-    // require('diagram-js-direct-editing'),
+    require('../../diagram-js-direct-editing').default,
     // require('diagram-js/lib/features/context-pad'),
     require('@diagram-ts/features/context-pad'),
     // require('diagram-js/lib/features/selection'),
