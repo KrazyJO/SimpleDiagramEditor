e46d86a581fa98b5e6337b5fbe9f7023790607d7
joern.otte@gmx.net <joern.otte@gmx.net>
2018-07-16 12:26:43 +0200

    #2 test transform to skeletal structure

--- src/transformer/Transformer.ts
+++ src/transformer/Transformer.ts
@@ -4,7 +4,20 @@ class Transformer {
      * transform the object of this class to an xml string
      */
     public transformJsonToDiagram(obj : object) : string {
-        return 'abc';
+        let result = '<?xml version="1.0" encoding="UTF-8"?>'; // add xml definition
+        result = this.addLineBreakToString(result);
+        //add root
+        result += '<sde:SimpleDebugEditorGraph id="eg_1" xmlns:sde="https://seblog.cs.uni-kassel.de/sde" xmlns:sdedi="https://seblog.cs.uni-kassel.de/sdedi" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">';
+        result = this.addLineBreakToString(result);
+        
+
+        //close root
+        result += '</sde:SimpleDebugEditorGraph>';
+        return result;
+    }
+
+    private addLineBreakToString(str : string) : string {
+        return str += '\n';
     }
 
     public transformDiagramToJson(str : string) : object {
--- test/spec/transformer/JsonToXmlSpec.js
+++ test/spec/transformer/JsonToXmlSpec.js
@@ -20,10 +20,16 @@ describe('transformer', () => {
                 a : "12",
                 b : 45
             };
+
+            //yes, it is ugly...
+            const expectedResult = 
+`<?xml version="1.0" encoding="UTF-8"?>
+<sde:SimpleDebugEditorGraph id="eg_1" xmlns:sde="https://seblog.cs.uni-kassel.de/sde" xmlns:sdedi="https://seblog.cs.uni-kassel.de/sdedi" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+</sde:SimpleDebugEditorGraph>`;
             
             let oTransformer = new Transformer();
             let result = oTransformer.transformJsonToDiagram(oObjectToTransform);
-            expect(result).to.equal('abc');
+            expect(result).to.equal(expectedResult);
         });
 
     });
