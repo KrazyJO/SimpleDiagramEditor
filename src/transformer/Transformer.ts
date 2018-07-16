03a06272bb4f568d4b554dcf6759986e3c02a9a0
joern.otte@gmx.net <joern.otte@gmx.net>
2018-07-16 13:26:38 +0200

    #2 refactored tranformer

--- src/transformer/Transformer.ts
+++ src/transformer/Transformer.ts
@@ -2,43 +2,53 @@ class Transformer {
     
 
     private result : string;
+    private diagram : string;
+    private diagramInterchange : string;
+
 
     /**
      * transform the object of this class to an xml string
      */
     public transformJsonToDiagram(obj : object) : string {
+        this.diagramInterchange = '';
+        this.diagram = '';
         this.result = '<?xml version="1.0" encoding="UTF-8"?>'; // add xml definition
         this.addLineBreakToResult();
         //add root
         this.result += '<sde:SimpleDebugEditorGraph id="eg_1" xmlns:sde="https://seblog.cs.uni-kassel.de/sde" xmlns:sdedi="https://seblog.cs.uni-kassel.de/sdedi" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">';
         this.addLineBreakToResult(); 
         
-        this.result += this.transformObject(obj, 'root');
+        // this.result += this.transformObject(obj, 'root');
+        this.transformObject(obj, 'root');
+        this.result += this.diagram;
+
+        this.diagram += this.diagramInterchange;
 
         //close root
         this.result += '</sde:SimpleDebugEditorGraph>';
         return this.result;
     }
 
-    private transformObject(obj : object, name : string) : string {
+    private transformObject(obj : object, name : string) : void {
         //add node
-        let innerResult = `<sde:Node id="node_1" name="${name}">`;
-        innerResult = this.addLineBreakToString(innerResult);
+        this.diagram += `<sde:Node id="node_1" name="${name}">`;
+        this.addLineBreakToDiagram();
         
+        //add object properties
         let keys = Object.keys(obj);
         
         for (let i = 0; i < keys.length; i++)
         {
             if (typeof obj[keys[i]] === 'number' ) {
-                innerResult += `<sde:members>
+                this.diagram += `<sde:members>
 ${keys[i]}
 </sde:members>`
-                innerResult += '\n';
+                this.addLineBreakToDiagram();
             } else if (typeof obj[keys[i]]  === 'string' ) {
-                innerResult += `<sde:members>
+                this.diagram += `<sde:members>
 ${keys[i]}
 </sde:members>`
-                innerResult += '\n';
+                this.addLineBreakToDiagram();
             } else if (typeof (obj[keys[i]] === 'boolean')) {
 
             }
@@ -46,18 +56,33 @@ ${keys[i]}
 
             }
         }
-        innerResult += '</sde:Node>'
-        innerResult = this.addLineBreakToString(innerResult);
-        return innerResult;
+        this.diagram += '</sde:Node>'
+        this.addLineBreakToDiagram();
+
+        //add more objects to diagram
+        for (let i = 0; i < keys.length; i++)
+        {
+            if (typeof obj[keys[i]] === 'object') {
+                this.transformObject(obj[keys[i]], keys[i]);
+            }
+        }
     }
 
     private addLineBreakToResult() {
         this.result += '\n';
     }
 
-    private addLineBreakToString(str : string) : string {
-        return str += '\n';
+    private addLineBreakToDiagram() {
+        this.diagram += '\n';
     }
+
+    // private addLineBreakToDiagramInterchange() {
+    //     this.diagramInterchange += '\n';
+    // }
+
+    // private addLineBreakToString(str : string) : string {
+    //     return str += '\n';
+    // }
 
     public transformDiagramToJson(str : string) : object {
         return null;
